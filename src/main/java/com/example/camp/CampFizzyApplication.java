5a01a28c43500fa696a6a34cb882352ef1c662e1
Jay M <jaymansmann@yahoo.com>
2022-08-08 12:11:27 -0400

    Initial commit

---
+++ src/main/java/com/example/camp/CampFizzyApplication.java
@@ -0,0 +1,13 @@
+package com.example.camp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampFizzyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampFizzyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/camp/CampFizzyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.camp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampFizzyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
